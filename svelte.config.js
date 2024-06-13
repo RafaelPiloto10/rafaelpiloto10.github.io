904a3ffe0c68458c7246e4d2dac28ed809400b80
Rafael Piloto <31188361+RafaelPiloto10@users.noreply.github.com>
2024-06-13 11:49:17 -0700

    switch to auto adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@svelte/adapter-vercel';
+import adapter from '@svelte/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
