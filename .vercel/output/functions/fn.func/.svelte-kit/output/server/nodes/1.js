350ae8fd83527c1325c8082cc8788e38752476af
Rafael Piloto <31188361+RafaelPiloto10@users.noreply.github.com>
2024-06-17 23:33:02 -0700

    remove .vercel folder

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.bb50d423.js","_app/immutable/chunks/scheduler.34300998.js","_app/immutable/chunks/index.59673f64.js","_app/immutable/chunks/entry.d886de78.js","_app/immutable/chunks/index.ca9ee2c1.js"];
-export const stylesheets = [];
-export const fonts = [];
--- .vercel/output/static/_app/immutable/entry/start.ef78d768.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.d886de78.js";export{t as start};
